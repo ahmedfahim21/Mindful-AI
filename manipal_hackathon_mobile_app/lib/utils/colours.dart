d4a378ef22d297902d05ba61bc047091cc5280c5
AbhishekSatpathy4848 <abhisheksatpathy.211it002@nitk.edu.in>
2023-10-20 04:09:51 +0530

    added questions and colours constants

--- manipal_hackathon_mobile_app/lib/utils/colours.dart
+++ manipal_hackathon_mobile_app/lib/utils/colours.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
-const backgroundColour = Color.fromRGBO(244, 246, 245, 1);
+const backgroundColour = Color.fromRGBO(236, 242, 239, 1);
+const greenColour = Color.fromRGBO(103, 201, 152, 0.8);
+const black = Colors.black;
+const blueColour = Color.fromRGBO(81, 151, 232, 1);
+Color white = Colors.white.withOpacity(0.7);
