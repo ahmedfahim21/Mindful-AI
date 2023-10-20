a203e6341c047476ea7e5ce1b469f268bbb278b9
ahmedfahim21 <ahmed.fahim0207@gmail.com>
2023-10-20 15:52:31 +0530

    dashboard: student graphs

--- dashboard/src/components/BarChart.jsx
+++ dashboard/src/components/BarChart.jsx
@@ -4,25 +4,25 @@ import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
 
 
-function BarChart() {
+function BarChart(data) {
 
 
-  const data = { 
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
-    datasets: [
-      {
-        label: 'Quantity Sold',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
+  // const data = { 
+  //   labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+  //   datasets: [
+  //     {
+  //       label: 'Quantity Sold',
+  //       data: [12, 19, 3, 5, 2, 3],
+  //       backgroundColor: [
+  //         'rgba(255, 99, 132, 0.2)',
+  //         'rgba(54, 162, 235, 0.2)',
+  //         'rgba(255, 206, 86, 0.2)',
+  //         'rgba(75, 192, 192, 0.2)',
 
-        ]
-      },
-    ],
-  };
+  //       ]
+  //     },
+  //   ],
+  // };
 
   const options = {
     scales: {
@@ -40,9 +40,9 @@ function BarChart() {
 
 
   return (
-    <>
-      <Bar data={data} options={options} />
-    </>
+    <div className='w-[400px]'>
+      <Bar data={data.data} options={options} />
+    </div>
   )
 }
 
