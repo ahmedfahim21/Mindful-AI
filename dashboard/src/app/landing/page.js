4f0a3fe48edaf29bb7973cb5024a2f7a129e8dc9
Shubham-Rasal <bluequbits@gmail.com>
2023-10-21 15:16:20 +0530

    added footer

--- dashboard/src/app/landing/page.js
+++ dashboard/src/app/landing/page.js
@@ -11,6 +11,20 @@ const Home = () => {
       <Navbar />
       <Hero />
       <About />
+
+
+      <footer className="flex flex-col items-center justify-center text-lg w-full h-20 border-t">
+        <a
+          className="flex items-center justify-center"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+          Powered by{" "} 
+          <img src="/logo.png" alt="Our Logo" className="h-4 ml-2 pr-3" />
+          and made with ❤️ by the Just Not Ideas
+        </a>
+      </footer>
+
     </div>
   )
 }
